aa6ba8fe4350b7242f7d6445be1787cdb73ab4a9
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2018-07-12 22:19:36 -0700

    "Removed Debug Code"

--- puzzle.py
+++ puzzle.py
@@ -1,14 +1,3 @@
-test_puzzle = [
-    [11,12,13,14,15,16,17,18,19],
-    [21,22,23,24,25,26,27,28,29],
-    [31,32,33,34,35,36,37,38,39],
-    [41,42,43,44,45,46,47,48,49],
-    [51,52,53,54,55,56,57,58,59],
-    [61,62,63,64,65,66,67,68,69],
-    [71,72,73,74,75,76,77,78,79],
-    [81,82,83,84,85,86,87,88,89],
-    [91,92,93,94,95,96,97,98,99],]
-
 puzzle = [
     [0,0,0,2,6,0,7,0,1],
     [6,8,0,0,7,0,0,9,0],
@@ -99,15 +88,11 @@ def checkCoordinatesForValue(x,y, value):
         return True
 def setup():
     getBlankSpacesList()
+
 def traverse(startPos, iValue):
-    #print("Traverse Started ")
-    #print(len(blankspaces))
+    puzzleFinish = False
     for item, i in zip(blankspaces[startPos:], range(iValue,len(blankspaces)+1)):
         for testNumber in range(item[2]+1,10):
-            #print("**********INFO**********")
-            #print("item: " + str(item))
-            #print("i: " + str(i))
-            #print("testNumber: " + str(testNumber))
             if checkCoordinatesForValue(item[0],item[1],testNumber):
                 setCoordinate(item[0],item[1],testNumber)
                 for i in range(0,9):
@@ -117,28 +102,28 @@ def traverse(startPos, iValue):
                     item[2] = testNumber
                 else:
                     item[2] = 0
-                #print(str(testNumber) + " Inserted at " + str(item[0]) + "," + str(item[1]) + " and item3 is " + str(item[2]))
                 if item[0] is 8 and item[1] is 8:
-                    for i in range(0,9):
-                        print(puzzle[i])
-                    print()
+                    puzzleFinish = True
                     exit()
                 break
             else:
-                #print(str(testNumber) + " already exists at location " + str(item[0]) + "," + str(item[1]))
                 if testNumber is 9:
-                    #print("NOW BREAKING!!" + str(i-1))
                     item[2] = 0
                     setCoordinate(item[0],item[1],0)
-                    traverse(i-1, i-1)
-                    break
-                    return
+                    if not(puzzleFinish):
+                        traverse(i-1, i-1)
+                    else:
+                        break
+        if puzzleFinish:
+            print("break")
+            break
 
 for i in range(0,9):
     print(puzzle[i])
 print()
 setup()
 traverse(0,0)
+print("Finished Sudoku Puzzle")
 
 #print(getVerticalListForCoordinate(5))
 #print(getHorizontalListForCoordinate(2))
